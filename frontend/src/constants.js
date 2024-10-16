95260e76ccd9b0e60a7f79aeac1bbd0534e95688
Cheerla Ratan Kalpa Sai <ratankalpasai@gmail.com>
2024-10-17 03:29:18 +0530

    Template Code Commit

--- frontend/src/constants.js
+++
@@ -1,5 +0,0 @@
-// frontend/src/constants.js
-export const BASE_URL = "";
-export const CUSTOMER_URL = "/api/customer";
-export const CENTER_URL = "/api/center";
-export const BOOKING_URL = "/api/booking";
