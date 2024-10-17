d36990dc26058e44589af872ae7d77b2f50e2956
Cheerla Ratan Kalpa Sai <ratankalpasai@gmail.com>
2024-10-17 06:41:42 +0530

    Update bookingModel.js

--- backend/models/bookingModel.js
+++ backend/models/bookingModel.js
@@ -6,9 +6,9 @@ const bookingSchema = new mongoose.Schema(
 		user: { type: String, required: true },
 		center: { type: String, required: true },
 		kind: { type: String, required: true },
-		cnt: { type: Number, required: true },
+		cnt: { type: String, required: true },
 		date: { type: String, required: true }, // Date is very annoying
-		time: { type: Number, required: true },
+		time: { type: String, required: true },
 	},
 	{
 		timestamps: true,
